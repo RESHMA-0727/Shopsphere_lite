572ddd2a7f58e15ad63bb069d1c9410496802b73
RESHMA-0727 <vishnumolakalareshmasri@gmail.com>
2026-04-16 18:29:18 +0530

    implement JWT authentication using HTTPBearer for protected routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app import models
@@ -10,6 +11,8 @@ router = APIRouter()
 
 SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
+
+security = HTTPBearer()
 
 # DB connection
 def get_db():
@@ -26,17 +29,20 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+# GET CURRENT USER (UPDATED)
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return payload["sub"]
+
 # SIGNUP
 @router.post("/signup")
 def signup(username: str, password: str, db: Session = Depends(get_db)):
-    # check if user already exists
     existing_user = db.query(models.User).filter(models.User.username == username).first()
-    
     if existing_user:
         return {"error": "Username already exists"}
 
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
     user = models.User(username=username, password=hashed_password)
     db.add(user)
     db.commit()
@@ -53,7 +59,5 @@ def login(username: str, password: str, db: Session = Depends(get_db)):
     if not user or user.password != hashed_input:
         return {"error": "Invalid credentials"}
 
-    # create JWT token
     token = create_access_token({"sub": user.username})
-
     return {"access_token": token}
