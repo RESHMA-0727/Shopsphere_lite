0eb20d5b1ca3a8f7082fa8e88a88d0f09b473ea1
RESHMA-0727 <vishnumolakalareshmasri@gmail.com>
2026-04-27 17:55:56 +0530

    integrate profile route into main application

--- app/main.py
+++ app/main.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
 from app.database import engine
 from app import models
+
+# Routers
 from app.auth.routes import router as auth_router
+from app.auth.profile import router as profile_router
 from app.products.routes import router as product_router
 from app.orders.routes import router as cart_router
 from app.orders.order_routes import router as order_router
 
+# Create DB tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Include routers
 app.include_router(auth_router)
+app.include_router(profile_router)
 app.include_router(product_router)
 app.include_router(cart_router)
 app.include_router(order_router)
 
+# Root endpoint
 @app.get("/")
 def home():
-    return {"message": "ShopSphere Lite API Running Sucessfully !!🚀"}
-
+    return {"message": "ShopSphere Lite API Running Successfully 🚀"}
 
 
